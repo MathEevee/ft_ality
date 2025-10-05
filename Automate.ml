72bdd50d7f35d383518c1f4ad1c1b1c870b8f561
Mathilde De Oliveira <matde-ol@2A5.42angouleme.fr>
2025-10-05 16:50:53 +0200

    add print Blink and colors

--- Automate.ml
+++ Automate.ml
@@ -57,11 +57,12 @@ let launch_Automate automate key_mapping =
 				let curr_state = Trie.next_state curr_state sym in
 				let combo = combo ^ "," ^ sym in
 				Print.print_Combo combo;
-				List.iter print_endline curr_state.values;
+				Print.combo_Found curr_state.values;
 				if curr_state.children = [] then begin
 					print_endline "\x1b[38;5;140mNo more possible combo\x1b[0m";
 					in_Automate automate alphabet "" end
-				else
+				else begin
 					in_Automate curr_state alphabet combo
+					end
 	in 
 	in_Automate automate key_mapping ""
--- Print.mli
+++ Print.mli
@@ -3,3 +3,4 @@ val print_Warning : string -> unit
 val print_Assigned : string -> unit
 val print_Key_Mapping : (Tsdl.Sdl.keycode * string) list -> unit
 val print_Combo : string -> unit
+val print_Combo_Found : string list -> unit
